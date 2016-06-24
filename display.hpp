9989479a19ba890277f765b287effddaf21c2f05
Antony Alkmim <antony-alkmim@hotmail.com>
2016-06-24 01:43:24 -0300

    Write data in LCD display

--- display.hpp
+++ display.hpp
@@ -1,13 +1,10 @@
-/***
-*	Display
-*	Author: Antony Alkmim
-*/
+
 
 /*
  * Enviar caracteres e comandos ao LCD com via de dados de 8 bits.
  * c é o dado e cd indica se é instrução ou caractere (0 ou 1).
  **/
-void cmd_LCD(unsigned char c, char cd);
+void cmd_LCD(unsigned char c, int cd);
 /*	Inicialização	do	LCD	com	via	de	dados	de	8	bits	*/
 void inic_LCD_8bits();
 /*	Escrita	no	LCD	–	dados	armazenados	na	RAM	*/
@@ -15,44 +12,90 @@ void escreve_LCD(char *c);
 /*	Escrita	no	LCD	–	dados	armazenados	na	FLASH	*/
 void escreve_LCD_Flash(const char *c);
 
+void lcd_clear();
 
-void inic_LCD_8bits(){
-	PORTB = 2;
-	_delay_us(40);
-	PORTD = (7 << 3); //function set
-	_delay_us(39);
-	PORTD = (7 << 3); //function set
-	_delay_us(37);
-	PORTD = (7 << 1); //display control
-	_delay_us(37);
-	PORTD = 1; //display clear
-	_delay_ms(1.53);
-	PORTD = (2 << 1); //mode set
+void pulso();
+
+void pulso(){
+	_delay_us(1);
+	PORTA |= 1;
+	_delay_us(1);
+	PORTA &= ~(1);
+	_delay_us(45);
 }
 
-void cmd_LCD(unsigned char c, char cd){
-	//instrucao
-	if(cd == '0'){
-		PORTB &= ~(1 << 1); //modo de instrucao
-	}
-	//caractere
-	if(cd == '1'){
-		PORTB |= (1 << 1); //modo de dados
-	}
-	PORTD = c;
+void inic_LCD_8bits(){
+
+	_delay_us(40);
+	PORTA &= ~0x02; //negar RS
+	//PORTC = 0b00111011;
+	PORTC = 7 << 3;
+	pulso();
+
+	//function set
+	_delay_us(39);
+	PORTA &= ~0x02; //negar RS
+	PORTC = 7 << 3;//PORTC = 0b00111011;
+	pulso();
+
+	//display control
+	_delay_us(37);
+	PORTA &= ~0x02; //negar RS
+	//PORTC = 0b00001000;
+	PORTC = 0x0f;
+
+	//display clear
+	_delay_us(37);
+	PORTA &= ~0x02; //negar RS
+	PORTC = 0x01;
+	pulso();
+
+	//mode set
+	_delay_us(1600);
+	PORTA &= ~0x02; //negar RS
+	PORTC = 3 << 1;
+	pulso();
+}
+
+void cmd_LCD(unsigned char c, int cd){
+	//identifica se e dados ou comando
+	PORTA = cd == 1 ? (PORTA | 2) : (PORTA & ~2);
+	PORTC = c; //transfere o dado
 
 	//gerar pulso de habilitacao
-	_delay_us(1);
-	PORTB |= 1;
-	_delay_us(1);
-	PORTB &= 0b11111110;
-	_delay_us(45);
+	pulso();
+
+
+	if(c == 0x01 || c == 0x02){
+		_delay_us(1600);
+	}
 }
 
 void escreve_LCD(char *c){
 	int ch = 0;
 	while (c[ch] != 0) {
-		cmd_LCD(c[ch], '1');
+		cmd_LCD(c[ch], 1);
 		++ch;
 	}
 }
+
+
+void lcd_clear(){
+	PORTA &= ~2; //informa que é dados
+	PORTC = 0x01; //transfere o dado
+	_delay_ms(1.6);
+
+	//gerar pulso de habilitacao
+	pulso();
+}
+
+
+
+
+
+
+
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -8,56 +8,83 @@
 #include "adc.hpp"
 
 
-int temp; //valor da temperatura
-
 /**
-* Interrupcao para capturar as temperatudas 
-*/
-ISR(ADC_vect){
-	temp = ADC + (ADC*19)/256; //converte para graus celcius
-}
+ * PORTD/PORTC = 37,36,35,34,33,32,31,30
+ * PORTB/PORTA = 22,23
+ */
+
+int temperatura = 0; //valor da temperatura
+//
+//ISR(ADC_vect){
+//	temperatura = ADC + (ADC*19)/256;
+//}
 
 void setup(void){
-	DDRB = 0xFF;
-	DDRD = 0x00;
+	DDRA = 0xFF; //DDRB
+	DDRC = 0xFF; //DDRD
 
 	// Configure Display
 	//========================================
 	inic_LCD_8bits();
+	escreve_LCD("TEMP: ");
+	cmd_LCD(0xC0, 0);
+	escreve_LCD("LUMIN.: ");
 
-	// Configure Thermometer Sensor
-	//========================================
-	//Tensão de referência
-	select_reference(INTERNAL_1_1);
-	//Entrada analógica ADC0;
-	set_analog_channel(0);
-	//Divisão do clock da CPU 128
-	set_prescaler();
-	// Habilita a interrupção do ADC
-	enable_interrupt_adc();
-	//Habilita o ADC
-	enable(true);
+//	//Tensão de referência
+//	select_reference(INTERNAL_1_1);
+//	//Entrada analógica ADC0;
+//	set_analog_channel(0);
+//	//Divisão do clock da CPU 128
+//	set_prescaler();
+//	// Habilita a interrupção do ADC
+//	enable_interrupt_adc();
+//	//Habilita o ADC
+//	enable(true);
+//
+//	//Control and Status Register
+//	ADCSRB = 0x00;
+//
+//	//Configura a forma que o resultado da conversão do ADC
+//	set_result_adjust(RIGHT_ADJUST);
+//	//Habilita individualmente cada entrada	 do	ADC
+//	enable_input(0);
+//
+//	//Modo de auto disparo ativo
+//	start();
+}
 
-	//Control and Status Register
-	ADCSRB = 0x00;
+void writeTemperature(){
+	char str_temp[17];
 
-	//Configura a forma que o resultado da conversão do ADC
-	set_result_adjust(RIGHT_ADJUST);
-	//Habilita individualmente cada entrada	 do	ADC
-	enable_input(0);
+	cmd_LCD(0x80, 0); //vai para primeira linha
+	for(int i=0; i<7; i++) //mover cursor para coluna 7
+		cmd_LCD(0x14, 0);
 
-	//Modo de auto disparo ativo
-	start();
+	++temperatura;
+//	dtostrf((float)temperatura/10, 2, 1, str_temp);
+	dtostrf((float)temperatura, 2, 1, str_temp);
+	sprintf(str_temp, "%s *C", str_temp, 0xA7);
+	escreve_LCD(str_temp);
+}
+
+void writeLuminosity(){
+	char str_temp[17];
+
+	cmd_LCD(0xC0, 0); //vai para segunda linha
+	for(int i=0; i<7; i++) //mover cursor para coluna 8
+		cmd_LCD(0x14, 0);
+
+	++temperatura;
+	dtostrf((float)temperatura, 2, 1, str_temp);
+	sprintf(str_temp, "%s %%", str_temp);
+	escreve_LCD(str_temp);
 }
 
 void loop(void){
+	writeTemperature();
+	writeLuminosity();
+	_delay_ms(1000);
 
-	//parse temperature to char[]
-	char str_temp[6];
-	dtostrf((float) temp/10, 2, 3, str_temp);
-	
-	//print
-	//escreve_LCD("TESTE");
 }
 
 
