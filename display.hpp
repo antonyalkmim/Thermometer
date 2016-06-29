25636a806ab9a23f11bc9ea7efc1634ec7c1bfc6
Antony Alkmim <antony-alkmim@hotmail.com>
2016-06-29 19:10:57 -0300

    refactoring display

--- display.hpp
+++ display.hpp
@@ -3,33 +3,48 @@
  */
 #include <avr/pgmspace.h>
 
+#define LCD_COMMAND 0
+#define LCD_DATA 1
+
+#define LCD_CLEAR 0x01 //limpa o display
+
+//cursor commands
+#define LCD_CURSOR_RESET 0x02 //volta o cursor para primeira coluna e primeira linha
+#define LCD_CURSOR_ON 0x0E; //liga cursor
+#define LCD_CURSOR_OFF 0x0C; //desliga cursor
+#define LCD_CURSOR_LEFT 0x10; //desloca cursor para esquerda
+#define LCD_CURSOR_RIGHT 0x14 //desloca cursor para direita
+#define LCD_CURSOR_LN1 0x80 //cursor no inicio da linha 1
+#define LCD_CURSOR_LN2 0xC0 //cursor no inicio da linha 2
+
 /*
  * Enviar caracteres e comandos ao LCD com via de dados de 8 bits.
  * c é o dado e cd indica se é instrução ou caractere (0 ou 1).
  **/
-void cmd_LCD(unsigned char c, int cd);
-/*	Inicialização	do	LCD	com	via	de	dados	de	8	bits	*/
-void inic_LCD_8bits();
-/*	Escrita	no	LCD	–	dados	armazenados	na	RAM	*/
-void escreve_LCD(char *c);
-/*	Escrita	no	LCD	–	dados	armazenados	na	FLASH	*/
-void escreve_LCD_Flash(const char *c);
+void lcd_exec(unsigned char c, int cd);
+/* Inicialização do LCD com via de dados de 8 bits*/
+void lcd_init();
+/* Escrita no LCD – dados armazenados na RAM */
+void lcd_print(char *c);
+/* Escrita no LCD – dados armazenados na FLASH */
+void lcd_print_flash(const char *c);
+/* Executa um comando no display */
+void lcd_command(const uint8_t command);
 /* Gera o pulso para habilitar comanando */
-void pulso();
+void lcd_pulse();
 
-
-void inic_LCD_8bits(){
+void lcd_init(){
 
 	_delay_us(40);
 	PORTA &= ~0x02; //negar RS
 	PORTC = 7 << 3;
-	pulso();
+	lcd_pulse();
 
 	//function set
 	_delay_us(39);
 	PORTA &= ~0x02; //negar RS
 	PORTC = 7 << 3;
-	pulso();
+	lcd_pulse();
 
 	//display control
 	_delay_us(37);
@@ -40,22 +55,23 @@ void inic_LCD_8bits(){
 	_delay_us(37);
 	PORTA &= ~0x02; //negar RS
 	PORTC = 0x01;
-	pulso();
+	lcd_pulse();
 
 	//mode set
 	_delay_us(1600);
 	PORTA &= ~0x02; //negar RS
 	PORTC = 3 << 1;
-	pulso();
+	lcd_pulse();
 }
 
-void cmd_LCD(unsigned char c, int cd){
+void lcd_exec(unsigned char c, int cd){
+
 	//identifica se e dados ou comando
 	PORTA = cd == 1 ? (PORTA | 2) : (PORTA & ~2);
 	PORTC = c; //transfere o dado
 
 	//gerar pulso de habilitacao
-	pulso();
+	lcd_pulse();
 
 	//gerar delay extra para funcoes de limpar
 	if(cd == 0 && (c == 0x01 || c == 0x02)){
@@ -63,24 +79,28 @@ void cmd_LCD(unsigned char c, int cd){
 	}
 }
 
-void escreve_LCD(char *c){
+void lcd_command(const uint8_t command){
+	lcd_exec(command, LCD_COMMAND);
+}
+
+void lcd_print(char *c){
 	int ch = 0;
 	while (c[ch] != 0) {
-		cmd_LCD(c[ch], 1);
+		lcd_exec(c[ch], LCD_DATA);
 		++ch;
 	}
 }
 
-void escreve_LCD_Flash(const char *c){
+void lcd_print_flash(const char *c){
 	int ch = 0;
 	char word = pgm_read_byte(&c[ch]);
 	while (word != 0) {
-		cmd_LCD(word, 1);
+		lcd_exec(word, LCD_DATA);
 		word = pgm_read_byte(&c[++ch]);;
 	}
 }
 
-void pulso(){
+void lcd_pulse(){
 	_delay_us(1);
 	PORTA |= 1;
 	_delay_us(1);
